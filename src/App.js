095e05153066f579ae53429c565f413155489de9
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-06-21 13:17:48 -0300

    react-router-dom, routes, styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Logon from "./pages/logon";
+import GlobalStyle from "./styles/globalStyle";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/logon" element={<Logon />} />
+        <Route path="/" element={<div>helloworld</div>} />
+      </Routes>
+      <GlobalStyle />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/logon/index.js
@@ -0,0 +1,7 @@
+import { Container } from "./styles";
+
+function Logon() {
+  return <Container>Logon</Container>;
+}
+
+export default Logon;
