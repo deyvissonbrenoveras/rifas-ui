2c20424065b2e86acac37712e9cbd0f466d34fb0
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-07-13 22:59:12 -0300

    feat: common components

---
+++ src/components/common/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Label = styled.label`
+  display: block;
+  margin: 10px 0;
+`;
+export const ErrorMessage = styled.span`
+  color: #ff0000;
+`;
