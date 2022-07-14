a4906fe34ee424dbc641e386a3b0183173c31eff
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-07-13 22:56:39 -0300

    feat: raffleValidationSchema

---
+++ src/validations/index.js
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+
+export const raffleValidationSchema = Yup.object().shape({
+  title: Yup.string()
+    .max(100, "Máximo de 100 caracteres permitidos")
+    .required("O título é obrigatório"),
+  description: Yup.string()
+    .max(150, "Máximo de 150 caracteres permitidos")
+    .required("A descrição é obrigatória"),
+  quotaExpirationDate: Yup.date().required(
+    "A data de expiração da quota é obrigatória"
+  ),
+  quotaPrice: Yup.number()
+    .positive("Insira um valor positivo")
+    .required("O preço da quota é obrigatório"),
+  quotaQuantity: Yup.number()
+    .positive("Insira um valor positivo")
+    .required("A quantidade de quotas é obrigatória"),
+  allowedQuotasPerPurchase: Yup.number()
+    .positive("Insira um valor positivo")
+    .required("A quantidade de quotas por compra é obrigatória"),
+  categoryId: Yup.number()
+    .positive("Insira um valor positivo")
+    .required("A categoria é obrigatória"),
+});
