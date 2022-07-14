0aeddd507bf2c02f0907231598e5011425e4e8eb
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-07-13 22:57:53 -0300

    refactor: add formik to create raffle

--- src/pages/dashboard/createRaffle/index.js
+++ src/pages/dashboard/createRaffle/index.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { PageHeader, Row, Col, Form, Input, Button, DatePicker } from "antd";
+import { PageHeader, Row, Col } from "antd";
+import { Form, SubmitButton } from "formik-antd";
+import { Formik } from "formik";
 import history from "../../../services/history";
 import { useDispatch } from "react-redux";
 import { createRaffleRequest } from "../../../redux/modules/raffle/actions";
 import { ImageUpload } from "../../../components/imageUpload";
+import { Input, DatePicker } from "../../../components";
+
+import { raffleValidationSchema } from "../../../validations";
+
 export default function CreateRaffle() {
   const dispatch = useDispatch();
 
-  function onFinish(data) {
+  function onSubmit(data, { setSubmitting }) {
     dispatch(createRaffleRequest(data));
+    setSubmitting(false);
   }
 
   return (
@@ -18,98 +25,71 @@ export default function CreateRaffle() {
         title="Criar rifa"
         subTitle="crie uma rifa"
       />
-
-      <Form
-        onFinish={onFinish}
-        initialValues={{
-          categoryId: 1,
-        }}
-      >
-        <Row>
-          <Col xs={24}>
-            <ImageUpload name="firstImageId" label="imagem 1" />
-          </Col>
-          <Col xs={24}>
-            <ImageUpload name="secondImageId" label="imagem 2" />
-          </Col>
-          <Col xs={24}>
-            <ImageUpload name="thirdImageId" label="imagem 3" />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={24} lg={8}>
-            <Form.Item
-              name="title"
-              rules={[{ required: true, message: "O título é obrigatório!" }]}
-            >
-              <Input placeholder="Título" />
-            </Form.Item>
-            <Form.Item
-              name="description"
-              rules={[
-                { required: true, message: "A descrição é obrigatória!" },
-              ]}
-            >
-              <Input.TextArea placeholder="Descrição" rows={5} />
-            </Form.Item>
-            <Form.Item
-              name="quotaExpirationDate"
-              rules={[{ required: true, message: "O título é obrigatório!" }]}
-            >
-              <DatePicker placeholder="Data da expiração da quota" />
-            </Form.Item>
-            <Form.Item
-              name="quotaPrice"
-              rules={[{ required: true, message: "O preço é obrigatório!" }]}
-            >
-              <Input
-                placeholder="Preço da quota"
-                type="number"
-                prefix="R$"
-                suffix="Reais"
-              />
-            </Form.Item>
-            <Form.Item
-              name="quotaQuantity"
-              rules={[
-                {
-                  required: true,
-                  message: "A quantidade de quotas é obrigatória!",
-                },
-              ]}
-            >
-              <Input placeholder="Quantidade de quotas" type="number" />
-            </Form.Item>
-            <Form.Item
-              name="allowedQuotasPerPurchase"
-              rules={[
-                {
-                  required: true,
-                  message:
-                    "A quantidade de quotas permitidas por compra é obrigatória!",
-                },
-              ]}
-            >
-              <Input placeholder="Quotas permitidas por compra" type="number" />
-            </Form.Item>
-            <Form.Item
-              name="categoryId"
-              rules={[{ required: true, message: "categoryId é obrigatório!" }]}
-            >
-              <Input placeholder="categoryId" />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={24} lg={8}>
-            <Form.Item>
-              <Button block type="primary" htmlType="submit">
-                Salvar
-              </Button>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
+      <Row>
+        <Col xs={24} md={18} lg={12}>
+          <Formik
+            initialValues={{
+              firstImageId: 5,
+              title: "",
+              description: "",
+              quotaExpirationDate: "",
+              quotaPrice: "",
+              quotaQuantity: "",
+              allowedQuotasPerPurchase: "",
+              categoryId: 1,
+            }}
+            validationSchema={raffleValidationSchema}
+            onSubmit={onSubmit}
+            render={() => (
+              <Form>
+                {/* <ImageUpload
+                  label="Insira as imagens"
+                  firstImageName="firstImageId"
+                  secondImageName="secondImageId"
+                  thirdImageName="thirdImageId"
+                /> */}
+                <Input name="title" placeholder="Título" label="Título" />
+                <Input
+                  name="description"
+                  placeholder="Descrição"
+                  label="Descrição"
+                />
+                <DatePicker
+                  name="quotaExpirationDate"
+                  label="Data da expiração da quota"
+                  placeholder="Data da expiração da quota"
+                />
+                <Input
+                  name="quotaPrice"
+                  placeholder="Preço da quota"
+                  label="Preço da quota"
+                  type="number"
+                  prefix="R$"
+                  suffix="Reais"
+                />
+                <Input
+                  name="quotaQuantity"
+                  placeholder="Quantidade de quotas"
+                  label="Quantidade de quotas"
+                  type="number"
+                />
+                <Input
+                  name="allowedQuotasPerPurchase"
+                  placeholder="Quotas permitidas por compra"
+                  label="Quotas permitidas por compra"
+                  type="number"
+                />
+                <Input
+                  name="categoryId"
+                  placeholder="Id de categoria"
+                  label="Id de categoria"
+                />
+                <SubmitButton>enviar</SubmitButton>
+              </Form>
+            )}
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
