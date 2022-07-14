6d1a55a9ccc5b5fba2e2e4171596f07cae451e9b
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-07-13 22:58:35 -0300

    reafactor: Upload component

--- src/components/imageUpload/index.js
+++ src/components/imageUpload/index.js
@@ -1,43 +1,91 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
-import { Upload, Input, Form } from "antd";
+import { useField } from "formik";
+import { Upload, Form } from "antd";
 import api from "../../services/api";
 
-export function ImageUpload({ name, label, ...rest }) {
+export function ImageUpload({
+  label,
+  firstImageName,
+  secondImageName,
+  thirdImageName,
+  defaultValueName,
+}) {
+  const [firstImageField, firstImageMeta, firstImageHelpers] =
+    useField(firstImageName);
+  const [secondImageField, secondImageMeta, secondImageHelpers] =
+    useField(secondImageName);
+  const [thirdImageField, thirdImageMeta, thirdImageHelpers] =
+    useField(thirdImageName);
+
+  const [defaultValueField, defaultValueMeta, defaultValueHelpers] =
+    useField(defaultValueName);
+
   const [loading, setLoading] = useState(false);
+  const [fileList, setFileList] = useState(defaultValueField.value);
+
+  useEffect(() => {
+    if (defaultValueField.value) {
+      setFileList(defaultValueField.value);
+    }
+  }, [defaultValueField.value]);
+
+  useEffect(() => {
+    if (fileList) {
+      const firstImageId = fileList[0] ? fileList[0].uid : null;
+      const secondImageId = fileList[1] ? fileList[1].uid : null;
+      const thirdImageId = fileList[2] ? fileList[2].uid : null;
+
+      firstImageHelpers.setValue(firstImageId);
+      secondImageHelpers.setValue(secondImageId);
+      thirdImageHelpers.setValue(thirdImageId);
+    }
+  }, [fileList]);
+
+  function setData(image) {
+    setFileList([
+      ...fileList,
+      {
+        uid: image.id || "",
+        name: image.filename || "",
+        response: "200",
+        status: "done",
+        url: image.url || "",
+      },
+    ]);
+  }
 
   async function handleChange(e) {
     setLoading(true);
     const data = new FormData();
     data.append("image", e.file);
     const response = await api.post("images", data);
-    e.onSuccess(response.data, e.file);
+    setData(response.data);
+    // e.onSuccess(response.data, e.file);
+    setLoading(false);
   }
 
   return (
     <>
-      <Form.Item
-        name={name}
-        label={label}
-        getValueFromEvent={(e) => {
-          if (e.file.status === "done") {
-            setLoading(false);
-            return e.file.response.id;
-          }
-        }}
-      >
+      <label>
+        {label}
         <Upload
           listType="picture-card"
-          maxCount={1}
+          maxCount={3}
           customRequest={handleChange}
-          {...rest}
+          fileList={fileList}
+          onChange={(e) => {
+            if (e) {
+              setFileList(e.fileList);
+            }
+          }}
         >
           <div>
             {loading ? <LoadingOutlined /> : <PlusOutlined />}
             <div style={{ marginTop: 8 }}>Upload</div>
           </div>
         </Upload>
-      </Form.Item>
+      </label>
     </>
   );
 }
