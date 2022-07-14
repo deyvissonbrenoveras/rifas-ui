fb0fb19dd2d32451b89c414f7c22a2030751474a
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-07-13 22:57:19 -0300

    reafactor: Update raffle responsiviness

--- src/pages/dashboard/updateRaffle/index.js
+++ src/pages/dashboard/updateRaffle/index.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import moment from "moment";
-import { PageHeader } from "antd";
+import { PageHeader, Row, Col } from "antd";
 import { Form, SubmitButton, DatePicker } from "formik-antd";
 import history from "../../../services/history";
 import { useDispatch, useSelector } from "react-redux";
 import { loadRaffleRequest } from "../../../redux/modules/raffle/actions";
-import { ImageUpload } from "../../../components/imageUpload new";
+import { ImageUpload } from "../../../components/imageUpload";
 import { Formik } from "formik";
-import Input from "../../../components/input";
+import { Input } from "../../../components";
 
 export default function UpdateRaffle({ match }) {
   const dispatch = useDispatch();
@@ -58,58 +57,62 @@ export default function UpdateRaffle({ match }) {
         title="Atualizar rifa"
         subTitle="Visualize e altere a rifa"
       />
-      <Formik
-        initialValues={raffleToUpdate}
-        enableReinitialize
-        onSubmit={onSubmit}
-        render={() => (
-          <Form>
-            <ImageUpload
-              label="Insira as imagens"
-              firstImageName="firstImageId"
-              secondImageName="secondImageId"
-              thirdImageName="thirdImageId"
-              defaultValueName="fileList"
-            />
-            <Input name="title" placeholder="Título" label="Título" />
-            <Input
-              name="description"
-              placeholder="Descrição"
-              label="descrição"
-            />
-            <DatePicker
-              name="quotaExpirationDate"
-              placeholder="Data da expiração da quota"
-            />
-            <Input
-              name="quotaPrice"
-              placeholder="Preço da quota"
-              label="Preço da quota"
-              type="number"
-              prefix="R$"
-              suffix="Reais"
-            />
-            <Input
-              name="quotaQuantity"
-              placeholder="Quantidade de quotas"
-              label="Quantidade de quotas"
-              type="number"
-            />
-            <Input
-              name="allowedQuotasPerPurchase"
-              placeholder="Quotas permitidas por compra"
-              label="Quotas permitidas por compra"
-              type="number"
-            />
-            <Input
-              name="categoryId"
-              placeholder="Id de categoria"
-              label="Id de categoria"
-            />
-            <SubmitButton>enviar</SubmitButton>
-          </Form>
-        )}
-      />
+      <Row>
+        <Col xs={24} md={18} lg={12}>
+          <Formik
+            initialValues={raffleToUpdate}
+            enableReinitialize
+            onSubmit={onSubmit}
+            render={() => (
+              <Form>
+                <ImageUpload
+                  label="Insira as imagens"
+                  firstImageName="firstImageId"
+                  secondImageName="secondImageId"
+                  thirdImageName="thirdImageId"
+                  defaultValueName="fileList"
+                />
+                <Input name="title" placeholder="Título" label="Título" />
+                <Input
+                  name="description"
+                  placeholder="Descrição"
+                  label="descrição"
+                />
+                <DatePicker
+                  name="quotaExpirationDate"
+                  placeholder="Data da expiração da quota"
+                />
+                <Input
+                  name="quotaPrice"
+                  placeholder="Preço da quota"
+                  label="Preço da quota"
+                  type="number"
+                  prefix="R$"
+                  suffix="Reais"
+                />
+                <Input
+                  name="quotaQuantity"
+                  placeholder="Quantidade de quotas"
+                  label="Quantidade de quotas"
+                  type="number"
+                />
+                <Input
+                  name="allowedQuotasPerPurchase"
+                  placeholder="Quotas permitidas por compra"
+                  label="Quotas permitidas por compra"
+                  type="number"
+                />
+                <Input
+                  name="categoryId"
+                  placeholder="Id de categoria"
+                  label="Id de categoria"
+                />
+                <SubmitButton>enviar</SubmitButton>
+              </Form>
+            )}
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
