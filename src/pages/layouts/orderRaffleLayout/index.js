bd86192c1a142c5a076e9e061825b921f52cbeaf
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2022-07-19 08:16:00 -0300

    refactor: increase layout height

--- src/pages/layouts/orderRaffleLayout/index.js
+++ src/pages/layouts/orderRaffleLayout/index.js
@@ -3,15 +3,8 @@ import { Row, Col } from "antd";
 
 export default function OrderRaffleLayout({ children }) {
   return (
-    <Row align="center" style={{ height: "100%" }}>
-      <Col
-        xs={24}
-        sm={20}
-        md={16}
-        lg={12}
-        xl={10}
-        style={{ border: "1px solid red", height: "100%" }}
-      >
+    <Row align="center" style={{ height: "100vh" }}>
+      <Col xs={24} sm={20} md={16} lg={12} xl={10} style={{ height: "100%" }}>
         {children}
       </Col>
     </Row>
